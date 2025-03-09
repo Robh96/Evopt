772704639efb5cdd7235619777f91a93a70d9413
Roberto Hart-Villamil <rob.hartvillamil@gmail.com>
2025-03-09 11:08:47 +0000

    updated pypi image source

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='evopt',
-    version='0.132',
+    version='0.133',
     packages=find_packages(),
     install_requires=[
         'numpy>=2.2.1',
