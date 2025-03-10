bbe2372b2c152d25eefc0cf6a95868c4ac76826d
Roberto Hart-Villamil <rob.hartvillamil@gmail.com>
2025-03-10 14:22:37 +0000

    version upgrade

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='evopt',
-    version='0.133',
+    version='0.134',
     packages=find_packages(),
     install_requires=[
         'numpy>=2.2.1',
