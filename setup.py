27018aab399d65eddd9e3cb6fc8399d3973e1b89
Roberto Hart-Villamil <rob.hartvillamil@gmail.com>
2025-03-11 12:02:04 +0000

    -

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='evopt',
-    version='0.135',
+    version='0.136',
     packages=find_packages(),
     install_requires=[
         'numpy>=2.2.1',
